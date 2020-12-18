8a7f343835d5aeedfc99688f04806998a4c69107
Kellapu Yugender <kellapuyugender@gmail.com>
2020-12-18 18:11:53 +0530

    multiplication of two numbers

---
+++ add.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a1,a2,result;
+    printf("ENTER THE a1:");
+    scanf("%d",&a1);
+
+    printf("ENTER THE a2;");
+    scanf("%d",&a2);
+
+    result=a1*a2;
+
+    printf("the mul of %d and %d is %d",a1,a2,result);
+
+}
